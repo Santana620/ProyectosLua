2580ced44306a9af19c3f6587de6b9c6347f276e
Santana <santanacozarpablo@gmail.com>
2025-06-17 23:17:42 +0200

    Codigo Limpio y Optimizado (Actualziación: añadido codigo para la interacción json)

--- ProyectoLua.lua
+++ ProyectoLua.lua
@@ -11,6 +11,45 @@ local ListaDeTarea = { --Guardaremos todas las listas
         estado = "pendiente"
     }
 }
+--Codigo necesario para que pueda ser interactivo con json-------------------
+
+
+while true do
+    print("\n--- MENÚ ---")
+    print("1. Agregar tarea")
+    print("2. Completar tarea")
+    print("3. Mostrar tareas")
+    print("4. Guardar en JSON")
+    print("5. Salir")
+
+    io.write("Selecciona una opción: ")
+    local opcion = io.read()
+
+    if opcion == "1" then
+        io.write("Título: ")
+        local titulo = io.read()
+        io.write("Descripción: ")
+        local descripcion = io.read()
+        agregarTarea(titulo, descripcion)
+    elseif opcion == "2" then
+        io.write("Título de tarea a completar: ")
+        local titulo = io.read()
+        CompletarTareas(titulo)
+    elseif opcion == "3" then
+        imprimirTareas()
+    elseif opcion == "4" then
+        guardarEnJSON("tareas.json")
+    elseif opcion == "5" then
+        print("Saliendo...")
+        break
+    else
+        print("Opción no válida")
+    end
+end
+
+
+
+----------------------------------------------------------------------------
 function agregarTarea(titulo, descripcion)
     for i, tarea in ipairs(ListaDeTarea) do
             if tarea.titulo == titulo then
